7ec47cffa1fb3d4f36cdebd2d4d7b53eab4a5042
unknown <mainaoscar94@gmail.com>
2021-03-24 20:20:48 +0300

    done with firebase config

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA0eSD-_CcbGfzrEMG8ZquOiZsjP2e6tXg",
+    authDomain: "full-instagram-with-javascript.firebaseapp.com",
+    projectId: "full-instagram-with-javascript",
+    storageBucket: "full-instagram-with-javascript.appspot.com",
+    messagingSenderId: "719645799746",
+    appId: "1:719645799746:web:613d6f2c93d3a9811ec4db",
+    measurementId: "G-30NR3PR0DB"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
