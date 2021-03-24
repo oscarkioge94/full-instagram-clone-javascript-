6afd724fee67fec26d5fe6800947675571d1ee30
unknown <mainaoscar94@gmail.com>
2021-03-24 20:14:43 +0300

    done with register ui now working on firebase

---
+++ js/register.js
@@ -0,0 +1,6 @@
+document.querySelector('.login').onclick = ()=>{
+    var email = document.querySelector('.email').value;
+    var password = document.querySelector('.passwords').value;
+    console.log(email)
+    console.log(password)
+}
